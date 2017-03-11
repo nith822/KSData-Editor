bb5bbcb2a9990b0649909ca63e7258bda6819e05
nith822 <1harveygao@gmail.com>
2017-03-11 13:46:58 -0600

    Add KSData Editor which adds notes to user_kanji_info

---
+++ src/KSDataEditor.java
@@ -0,0 +1,99 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class KSDataEditor {
+	
+	Connection conn;
+	File ksdata;
+	File inputFile;
+	ArrayList<Integer> userKanjiCode;
+	ArrayList<String> inputLines;
+	
+    public static void main(String[] args) throws Exception {
+        Class.forName("org.sqlite.JDBC");
+        //ksdata file
+        File f = new File("C:/Users/Harvey/Desktop/sql/Backup-20170311132053-1.8.22.ksdata");
+        //input file
+        File x = new File("C:/Users/Harvey/Desktop/SubaHibi Sentences/SubaHibi 1.txt");
+        KSDataEditor test = new KSDataEditor(f.getPath(), x.getPath());
+        test.updateNotes();
+        test.conn.close();
+    }
+    
+    public KSDataEditor(String ksdataPath, String inputFile) throws SQLException, IOException {
+    	this.ksdata = new File(ksdataPath);
+    	this.inputFile = new File(inputFile);
+    	conn = DriverManager.getConnection("jdbc:sqlite:" + ksdata.getPath());
+    	scanInputLines();
+    	scanUserKanji();
+    }
+    
+    public void update(String note, int kanji_code) throws SQLException {
+    	 PreparedStatement pstmt = null;   
+         try {
+             pstmt = conn.prepareStatement(
+                         "UPDATE user_kanji_info" + " " +
+                         "SET notes = ?" + " " +
+                         "WHERE kanji_code = ?");
+
+             pstmt.setString(1, note);
+             pstmt.setInt(2, kanji_code);
+             pstmt.executeUpdate();
+             pstmt.close();
+         }	finally {
+             if (pstmt != null) pstmt.close();
+         }
+    }
+    
+    public boolean isKanji(Character c) {
+    	 return (c>=0x4e00 && c<0xa000);
+    }
+    
+    public void scanUserKanji() throws SQLException {
+    	userKanjiCode = new ArrayList<Integer>();
+    	//don't overwrite existing sentences
+    	ResultSet rs = conn.createStatement().executeQuery("SELECT kanji_code" + " " + "FROM user_kanji_info" + " " + "WHERE notes IS NULL");
+        while (rs.next()) {
+        	userKanjiCode.add(rs.getInt("kanji_code"));
+        }
+    	Collections.sort(userKanjiCode);
+    }
+    
+    public void scanInputLines() throws IOException {
+    	inputLines = new ArrayList<String>();
+    	BufferedReader reader = new BufferedReader( new InputStreamReader(new FileInputStream(inputFile), "UTF8"));
+    	String line;
+    	while((line = reader.readLine()) != null) {
+    		if(!line.isEmpty()) {
+    			inputLines.add(line);
+    		}
+    	}
+    	System.out.println(inputLines.toString());
+    	reader.close();
+    }
+    
+    public void updateNotes() throws SQLException {
+    	for(String line : inputLines) {
+    		for(Character c: line.toCharArray()) {
+    			if(isKanji(c)) {
+    				int arrayListIndex;
+    				if((arrayListIndex = Collections.binarySearch(userKanjiCode, (int)c)) >= 0) {
+    					int kanji_code = userKanjiCode.get(arrayListIndex);
+    					update(line, kanji_code);
+    					userKanjiCode.remove(arrayListIndex);
+    				}
+    			}
+    		}
+    	}
+    }
+  }
---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test {
+	
+	public static void main(String[]args) {
+		while(true) {
+		Scanner x = new Scanner(System.in);
+		System.out.println(Character.toString((char)x.nextInt()));
+		}
+	}
+	
+	
+}
